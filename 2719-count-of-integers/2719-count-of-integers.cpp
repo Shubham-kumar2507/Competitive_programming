e988845dfc0dd6e302ffdeaa631c3d4a75b4b1ec
Shubham kumar <148565356+Shubham-kumar2507@users.noreply.github.com>
2026-07-16 10:49:10 +0530

    Time: 12 ms (83.03%), Space: 9 MB (54.27%) - LeetHub

---
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -0,0 +1,32 @@
+const int MOD = 1e9 + 7;
+class Solution {
+    int dp[23][2][2][401];
+    int countStrings(int i, bool tight1, bool tight2, int sum, string& num1, string& num2){
+        if (sum < 0) return 0;
+        if (i == num2.size()) return 1;
+        if (dp[i][tight1][tight2][sum]!=-1) return dp[i][tight1][tight2][sum];
+
+        int lo = tight1 ? num1[i] - '0':0;
+        int hi = tight2 ? num2[i] - '0':9;
+
+        int count = 0;
+        for (int idx = lo; idx <= hi;idx++){
+            count = (count%MOD+countStrings(i + 1,
+                     tight1 & (idx == lo), tight2 & (idx == hi),
+                     sum - idx, num1, num2) % MOD) % MOD;
+        }
+
+        return dp[i][tight1][tight2][sum] = count;
+    }
+
+public:
+    int count(string num1, string num2, int min_sum, int max_sum) {
+        memset(dp, -1, sizeof(dp));
+        int lro= num2.length() - num1.length();
+        string num1ex = string(lro,'0')+num1;
+        int total = countStrings(0, 1, 1, max_sum, num1ex, num2);
+        int unnecessary = countStrings(0, 1, 1, min_sum - 1,num1ex,num2);
+        int ans = (total-unnecessary + MOD) % MOD;
+        return ans;
+    }
+};
