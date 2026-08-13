ead003998a9653457d8a853dd97029c0f5c9b917
Shubham kumar <148565356+Shubham-kumar2507@users.noreply.github.com>
2026-08-13 18:27:32 +0530

    Time: 7 ms (39.09%), Space: 91.7 MB (53.39%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -15,29 +15,35 @@ public:
     long sum=0;
     int trees(TreeNode* root){
         if(root==NULL) return 0;
-        sum +=root->val;
-        trees(root->left);
-        trees(root->right);
-        return sum; 
-    }
-    int find(TreeNode* root){
-        if(root ==NULL) return 0;
-
-        int lefts=find(root->left);
-        int rights=find(root->right);
+        //sum +=root->val;
+       int lefts= trees(root->left);
+       int rights  = trees(root->right);
         long subts=root->val+lefts+rights;
 
-        long s2= sum-subts;
+         long s2= sum-subts;
 
-        maxP=max(maxP,(subts*s2));
-        return subts;
+         maxP=max(maxP,(subts*s2));
+         return subts;
     }
+    // int find(TreeNode* root){
+    //     if(root ==NULL) return 0;
+
+    //     int lefts=find(root->left);
+    //     int rights=find(root->right);
+    //     long subts=root->val+lefts+rights;
+
+    //     long s2= sum-subts;
+
+    //     maxP=max(maxP,(subts*s2));
+    //     return subts;
+    //}
     int maxProduct(TreeNode* root) {
         if(root==NULL) return 0;
 
         maxP=0;
+       sum= trees(root);
         trees(root);
-        find(root);
+        //find(root);
         return maxP%(1000000007);
     }
 };
