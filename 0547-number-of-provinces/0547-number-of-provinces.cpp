1dbfbb2f92279e7c713c6e71cc6862e43df0b133
Shubham kumar <148565356+Shubham-kumar2507@users.noreply.github.com>
2026-09-01 18:02:16 +0530

    Time: 0 ms (100%), Space: 19.3 MB (82.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-void dfs(int u, vector<bool>& vis, unordered_map<int,vector<int>>& adj){
+int n;
+void dfs(int u, vector<bool>& vis, vector<vector<int>>& isC){
     vis[u]= true;
-    for(int &v:adj[u]){
-        if(!vis[v]){
-            dfs(v,vis,adj);
+    for(int v=0;v<n;v++){
+        if(!vis[v] && isC[u][v]==1){
+            dfs(v,vis,isC);
         }
     }
 }
     int findCircleNum(vector<vector<int>>& isC) {
-        int n=isC.size() , cnt=0;
-        unordered_map<int,vector<int>> adj;
+         n=isC.size() ;
+         int cnt=0;
+        //unordered_map<int,vector<int>> adj;
         vector<bool> vis(n,false);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isC[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(isC[i][j]==1){
+        //             adj[i].push_back(j);
+        //             adj[j].push_back(i);
+        //         }
+        //     }
+        // }
 
         for(int i=0;i<n;i++){
             if(!vis[i]){
             cnt++;
-            dfs(i,vis,adj);
+            dfs(i,vis,isC);
             }
         }
         return cnt;
