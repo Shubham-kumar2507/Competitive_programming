74ee504994df625250723d6f39d6f912392d2ceb
Shubham kumar <148565356+Shubham-kumar2507@users.noreply.github.com>
2026-09-26 15:54:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.91%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -4,7 +4,7 @@ public:
        
         string res="";
        for(char &ch: s){
-        if(!res.empty()&&(res.back()+32==ch || res.back()-32==ch)){
+        if(!res.empty()&& abs(res.back()-ch)==32){
             res.pop_back();
         }else res.push_back(ch);
        }
